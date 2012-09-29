301c996e83fbf1b56f87b4268ea9f35a80adea34
l1am9111 <l1am9111@googlemail.com>
2012-09-29 21:12:49 +0100

    fix

--- sampmap.cpp
+++ sampmap.cpp
@@ -1,34 +1,30 @@
-#include "sampmap.h"
-
-#include <sampgdk/amxplugin.h>
-#include <sampgdk/players.h>
-#include <sampgdk/samp.h>
-#include <sampgdk/wrapper.h>
-
 #include <cstdio>
 #include <cstring>
-#include "handler.h"
-#include <websocketpp.hpp>
-#include <boost/bind.hpp>
-#include <boost/foreach.hpp>
 #include <map>
 #include <iostream>
+#include <sstream>
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <libwebsockets.h>
 
-static logprintf_t logprintf;
+#include <sampgdk/a_players.h>
+#include <sampgdk/a_samp.h>
+#include <sampgdk/core.h>
+#include <sampgdk/plugin.h>
 
-using boost::asio::ip::tcp;
-using handler::web_handler;
-using namespace sampgdk;
+#define logprintf sampgdk_logprintf
 
-boost::asio::io_service io_service;
-handler::echo_server_handler_ptr samp_handler(new handler::web_handler());
-static HelloWorld theGameMode;
+//using namespace sampgdk;
 typedef std::map<int, struct playerdata> PlayerType;
 PlayerType players;
 std::stringstream webbuffer (std::stringstream::in | std::stringstream::out);
 int clients = 0;
 #define TICKS 15
 int ticks = 0;
+struct libwebsocket_context *context;
+
+static int callback_map(struct libwebsocket_context *context, struct libwebsocket *wsi, enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len);
 
 struct playerdata
 {
@@ -56,20 +52,54 @@ enum
 	EVENT_SPAWN
 };
 
-HelloWorld::HelloWorld() {
-	// Register our gamemode in order to catch events - if we don't do this
-	// somewhere none of the HelloWorld callbacks will be ever called.
-	this->Register();
+static int callback_map(struct libwebsocket_context *context, struct libwebsocket *wsi, enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len);
+
+struct libwebsocket_extension libwebsocket_internal_extensions[] = {
+	{ /* terminator */
+		NULL, NULL, 0
+	}
+};
+
+static struct libwebsocket_protocols protocols[] = {
+	{
+		"map",
+		callback_map,
+		0,
+	},
+	{
+		NULL, NULL, 0		/* End of list */
+	}
+};
+
+void websocket_send(struct libwebsocket *wsi)
+{
+	int length = webbuffer.str().length();
+	unsigned char *buf = new unsigned char[LWS_SEND_BUFFER_PRE_PADDING + length+1 + LWS_SEND_BUFFER_POST_PADDING];
+	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+	memcpy(p, webbuffer.str().c_str(), length);
+	p[length] = '\0';
+	libwebsocket_write(wsi, p, length+1, LWS_WRITE_TEXT);
+	delete[] buf;
 }
 
-HelloWorld::~HelloWorld() {}
+void websocket_send_to_all()
+{
+	int length = webbuffer.str().length();
+	unsigned char *buf = new unsigned char[LWS_SEND_BUFFER_PRE_PADDING + length+1 + LWS_SEND_BUFFER_POST_PADDING];
+	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+	memcpy(p, webbuffer.str().c_str(), length);
+	p[length] = '\0';
+	libwebsockets_broadcast(&protocols[0], p, length+1);
+	delete[] buf;
+}
 
-void HelloWorld::OnGameModeInit()
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit()
 {
 	players.clear();
+	return true;
 }
 
-bool HelloWorld::OnPlayerConnect(int playerid)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid)
 {
 	struct playerdata player;
 	char nick[MAX_PLAYER_NAME];
@@ -83,50 +113,50 @@ bool HelloWorld::OnPlayerConnect(int playerid)
 
 	webbuffer << CMD_PLAYERDATA;
 	webbuffer << boost::format("%1%,%2%,%3%,%4%,%5%,%6%;") % player.playerid % player.invehicle % player.x % player.y % player.nick % player.spawned;
-	samp_handler->send_to_all(webbuffer.str());
+	websocket_send_to_all();
 	webbuffer.str("");
 	return true;
 }
 
-bool HelloWorld::OnPlayerDisconnect(int playerid, int reason)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerDisconnect(int playerid, int reason)
 {
 	PlayerType::iterator player = players.find(playerid);
 	players.erase(player);
 	return true;
 }
 
-bool HelloWorld::OnPlayerSpawn(int playerid)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerSpawn(int playerid)
 {
 	float pos[3];
-	GetPlayerPos(playerid, pos[0], pos[1], pos[2]);
+	GetPlayerPos(playerid, &pos[0], &pos[1], &pos[2]);
 	players[playerid].x = static_cast<int>(pos[0]);
 	players[playerid].y = static_cast<int>(pos[1]);
 	players[playerid].spawned = true;
 	webbuffer << CMD_EVENT;
 	webbuffer << boost::format("%1%,%2%;") % EVENT_SPAWN % players[playerid].playerid;
-	samp_handler->send_to_all(webbuffer.str());
+	websocket_send_to_all();
 	webbuffer.str("");
 	return true;
 }
 
-bool HelloWorld::OnPlayerUpdate(int playerid)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerUpdate(int playerid)
 {
 	float pos[3];
-	GetPlayerPos(playerid, pos[0], pos[1], pos[2]);
+	GetPlayerPos(playerid, &pos[0], &pos[1], &pos[2]);
 	players[playerid].x = static_cast<int>(pos[0]);
 	players[playerid].y = static_cast<int>(pos[1]);
 	players[playerid].update = true;
 	return true;
 }
 
-bool HelloWorld::OnPlayerStateChange(int playerid, int newstate, int oldstate)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerStateChange(int playerid, int newstate, int oldstate)
 {
 	if(newstate == PLAYER_STATE_DRIVER || newstate == PLAYER_STATE_PASSENGER)
 	{
 		players[playerid].invehicle = true;
 		webbuffer << CMD_EVENT;
 		webbuffer << boost::format("%1%,%2%;") % EVENT_ENTERVEHICLE % players[playerid].playerid;
-		samp_handler->send_to_all(webbuffer.str());
+		websocket_send_to_all();
 		webbuffer.str("");
 	}
 	else if(newstate == PLAYER_STATE_ONFOOT)
@@ -134,13 +164,13 @@ bool HelloWorld::OnPlayerStateChange(int playerid, int newstate, int oldstate)
 		players[playerid].invehicle = false;
 		webbuffer << CMD_EVENT;
 		webbuffer << boost::format("%1%,%2%;") % EVENT_EXITVEHICLE % players[playerid].playerid;
-		samp_handler->send_to_all(webbuffer.str());
+		websocket_send_to_all();
 		webbuffer.str("");
 	}
 	return true;
 }
 
-bool HelloWorld::OnPlayerCommandText(int playerid, const std::string &cmdtext)
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid, const char *cmdtext)
 {
 	if (cmdtext == "/players")
 	{
@@ -155,24 +185,20 @@ bool HelloWorld::OnPlayerCommandText(int playerid, const std::string &cmdtext)
 	return false;
 }
 
-PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
+{
 	return SUPPORTS_VERSION | SUPPORTS_PROCESS_TICK;
 }
 
-PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppPluginData) {
-	logprintf = (logprintf_t)ppPluginData[PLUGIN_DATA_LOGPRINTF];
-	// Initialize the wrapper - this always should be done here.
-	Wrapper::GetInstance().Initialize(ppPluginData);
-	// Do not call any natives here - they are not yet prepared for use at this stage.
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppPluginData)
+{
+	sampgdk_initialize(ppPluginData);
 
-	tcp::endpoint endpoint(tcp::v6(), 4444);
-
-	websocketpp::server_ptr server(new websocketpp::server(io_service, endpoint, samp_handler));
-	server->add_host("127.0.0.1:4444");
-	server->set_max_message_size(0xFF);
-	server->start_accept();
-	server->set_alog_level(websocketpp::ALOG_OFF);
-	server->set_elog_level(websocketpp::LOG_OFF);
+	context = libwebsocket_create_context(4444, "", protocols, libwebsocket_internal_extensions, NULL, NULL, -1, -1, 0);
+	if (context == NULL)
+	{
+		fprintf(stderr, "Creating libwebsocket context failed\n");
+	}
 	return true;
 }
 
@@ -192,50 +218,54 @@ PLUGIN_EXPORT void PLUGIN_CALL ProcessTick()
 				}
 			}
 			webbuffer << ";";
-			samp_handler->send_to_all(webbuffer.str());
+			websocket_send_to_all();
 			webbuffer.str("");
 			ticks = 0;
 		}
 		ticks++;
 	}
-	io_service.poll();
+	libwebsocket_service(context, 0);
 }
 
-void web_handler::validate(websocketpp::session_ptr client) {}
-
-void web_handler::on_open(session_ptr client)
+static int callback_map(struct libwebsocket_context *context, struct libwebsocket *wsi, enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len)
 {
-	connections.push_back(client);
-	clients++;
+	int n;
+	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 +
+						  LWS_SEND_BUFFER_POST_PADDING];
+	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
 
-	webbuffer << CMD_PLAYERDATA;
-	BOOST_FOREACH(PlayerType::value_type &player, players)
+	switch (reason)
 	{
-		webbuffer << boost::format("%1%,%2%,%3%,%4%,%5%,%6%|") % player.second.playerid % player.second.invehicle % player.second.x % player.second.y % player.second.nick % player.second.spawned;
+
+	case LWS_CALLBACK_ESTABLISHED:
+		clients++;
+		webbuffer << CMD_PLAYERDATA;
+		BOOST_FOREACH(PlayerType::value_type &player, players)
+		{
+			webbuffer << boost::format("%1%,%2%,%3%,%4%,%5%,%6%|") % player.second.playerid % player.second.invehicle % player.second.x % player.second.y % player.second.nick % player.second.spawned;
+		}
+		webbuffer << ";";
+		websocket_send(wsi);
+		webbuffer.str("");
+		break;
+
+	case LWS_CALLBACK_CLOSED:
+		clients--;
+		break;
+
+	case LWS_CALLBACK_BROADCAST:
+		libwebsocket_write(wsi, (unsigned char*)in, len, LWS_WRITE_TEXT);
+		break;
+
+	default:
+		break;
 	}
-	webbuffer << ";";
-	client->send(webbuffer.str());
-	webbuffer.str("");
-}
 
-void web_handler::on_close(session_ptr client)
-{
-	std::list<session_ptr>::iterator it = std::find_if(connections.begin(), connections.end(), std::bind2nd( std::equal_to<session_ptr>(), client));
-	if(it == connections.end()) return;
-	connections.erase(it);
-	clients--;
-}
-
-void web_handler::on_message(websocketpp::session_ptr client, const std::string &msg)
-{
-}
-
-void web_handler::on_message(websocketpp::session_ptr client, const std::vector<unsigned char> &data)
-{
+	return 0;
 }
 
 PLUGIN_EXPORT void PLUGIN_CALL Unload()
 {
-	io_service.stop();
+	sampgdk_finalize();
 	return;
 }
